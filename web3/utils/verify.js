5e700f3443a6754398de717ca70f26fd720c8a74
tuagkcuf <jokersms222@gmail.com>
2023-05-19 14:06:43 +0300

    minor changes

--- web3/utils/verify.js
+++ web3/utils/verify.js
@@ -1,11 +1,11 @@
 import { run } from "hardhat"
 
-export const verify = async (contractAddress, args) => {
+export const verify = async (contractAddress, args, amount) => {
     console.log("Verifying contract...")
     try {
         await run("verify:verify", {
             address: contractAddress,
-            constructorArguments: args,
+            constructorArguments: [args, {value: amount}],
         })
     } catch (error) {
         if (error.message.toLowerCase().includes("already verified")) {
