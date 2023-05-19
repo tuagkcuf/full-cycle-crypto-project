7244a94a1b91845e745c506062e18fb1ed501f2c
tuagkcuf <jokersms222@gmail.com>
2023-05-19 09:32:51 +0300

    add hardhat config file and update hardhat config

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -3,10 +3,15 @@ import * as dotenv from "dotenv"
 dotenv.config()
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY
-const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "https://eth-sepolia.g.alchemy.com/v2/YOUR-API-KEY"
+const SEPOLIA_RPC_URL =
+    process.env.SEPOLIA_RPC_URL || "https://eth-sepolia.g.alchemy.com/v2/YOUR-API-KEY"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
 
 export const solidity = {
-    version: "0.8.9",
+    solidity: {
+        compilers: [{ version: "0.8.18" }, { version: "0.6.6" }],
+    },
     defaultNetwork: "sepolia",
     networks: {
         hardhat: {},
@@ -15,7 +20,27 @@ export const solidity = {
             accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
             saveDeployments: true,
             chaindId: 11155111,
+            blockConfirmations: 6,
         },
+    },
+    gasReporter: {
+        enabled: false,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+            1: 0,
+        },
+    },
+    mocha: {
+        timeout: 200000,
     },
     settings: {
         optimizer: {
---
+++ web3/helper-hardhat-config.js
@@ -0,0 +1,14 @@
+const networkConfig = {
+    11155111: {
+        name: "sepolia",
+        ethUsdPriceFeed: "your price feeds",
+    },
+    137: {
+        name: "polygon",
+        ethUsdPriceFeed: "your price feeds",
+    },
+}
+
+const developmentChains = ["hardhat", "localhost"]
+
+export { networkConfig, developmentChains }
