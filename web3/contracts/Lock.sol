d42db869f28d674f2961f8b79a15cb7809a5f22e
tuagkcuf <jokersms222@gmail.com>
2023-05-19 09:22:54 +0300

    update the contract and implement chainlink aggregatorv3

--- web3/contracts/Lock.sol
+++ web3/contracts/Lock.sol
@@ -1,34 +1,71 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import "./PriceConverter.sol";
 
+error Lock__TimeNotPassed();
+error Lock__InvalidUnlockTime();
+error Lock__NotOwner();
+error Lock__NotSendEnough();
+
+/**
+ * @title A contract for give me my money 
+ * @author tuagkcuf 
+ * @notice This contract is to show how to make me richer
+ * @dev This implements price feeds as our library for uint256 variables
+ */ 
 contract Lock {
+    using PriceConverter for uint256;
+
+    uint256 public constant MINIMUM_USD = 50 * 1e18;
     uint public unlockTime;
     address payable public owner;
+    AggregatorV3Interface private s_priceFeed;
+
 
     event Withdrawal(uint amount, uint when);
 
-    constructor(uint _unlockTime) payable {
+    /**
+     * @notice Get price feed address and time when to unlock the funded money
+     * @dev Pass price feed to the AggregatorV3Interface chainlink contract
+     * @param _unlockTime. When to unlock funds
+     * @param priceFeedAddress. Address of the AggregatorV3Interface of chainlink
+     */
+    constructor(uint _unlockTime, address priceFeedAddress) payable {
         require(
             block.timestamp < _unlockTime,
             "Unlock time should be in the future"
         );
-
+        s_priceFeed = AggregatorV3Interface(priceFeedAddress);
         unlockTime = _unlockTime;
         owner = payable(msg.sender);
     }
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+    /**
+     * @notice Funds the contract
+     * @dev If funded amount less than the minimum allowed usd to fund, than revert
+     */
+    function fund() public payable {
+        if (msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD) {
+            revert Lock__NotSendEnough();
+        }
+    }
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
+    /**
+     * @notice Withdraw funds
+     * @dev If unlock time not passed or if msg.sender != owner, than revert. Otherwise withdraw
+     */
+    function withdraw() public {
+        if (block.timestamp < unlockTime) {
+            revert Lock__TimeNotPassed();
+        }
+        if (msg.sender != owner) {
+            revert Lock__NotOwner();
+        }
 
         emit Withdrawal(address(this).balance, block.timestamp);
 
         owner.transfer(address(this).balance);
+        // owner.call{value: address(this).balance}("") - less gas consumption
     }
 }
