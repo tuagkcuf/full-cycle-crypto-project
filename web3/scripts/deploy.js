084e47e9c3352cb625ef8ce5d268deffb4203d02
tuagkcuf <jokersms222@gmail.com>
2023-05-19 08:19:04 +0300

    add tasks for me to go further

--- web3/scripts/deploy.js
+++ web3/scripts/deploy.js
@@ -4,15 +4,24 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-const hre = require("hardhat");
+// TODO: make a chainlink contract like vrf
+// TODO: rewrite contract myself with chainlink vrf
+// TODO: add mocks for chainlink
+// TODO: update script properly using fcc study and write it myself
+// TODO: update tests properly and write them myself
+// TODO: add etherscan verification script
+// TODO: convert everything to es6 module
+// TODO: find all remaining things that i didnt add here with fcc study
+// TODO: start with frontend side of this project
+import { ethers as _ethers } from "hardhat";
 
 async function main() {
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
   const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  const lockedAmount = _ethers.utils.parseEther("0.001");
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
+  const Lock = await _ethers.getContractFactory("Lock");
   const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
 
   await lock.deployed();
